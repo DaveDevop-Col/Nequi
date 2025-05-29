f2e72703c440ca62dd42c2c9d46d5b952dbdfb41
DaveDevop-Col <dsjs2307@gmail.com>
2025-05-29 02:08:55 +0000

    Actualizacion

--- fuente/clienteSupabase.js
+++ fuente/clienteSupabase.js
@@ -1,6 +1,7 @@
 // fuente/clienteSupabase.js
 // import { createClient } from '@supabase/supabase-js'; // Descomentar cuando se conecte a Supabase
 
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseClaveAnon = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
